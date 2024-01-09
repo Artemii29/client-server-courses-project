d711694c45db556bbbba973c5be9a9d05d9f664d
Artemii29 <artemii.kharitonov@bk.ru>
2024-01-09 16:26:42 +0300

    update run()

--- src/main/server/Server.java
+++ src/main/server/Server.java
@@ -1,10 +1,7 @@
 package main.server;
+import java.io.File;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -51,41 +48,43 @@ public class Server {
         @Override
         public void run() {
             try {
-                int read = clientSocket.getInputStream().read();
-                if (read == -1)
-                    System.out.println("disconnected");
-                else
-                    System.out.println(read);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    clientSocket.close();
-                    activeConnections.remove(clientSocket.getPort());
-                } catch (IOException e) {
-                    e.printStackTrace();
+                InputStream inputStream = clientSocket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                String message = reader.readLine();
+                File file = new File(message);
+                if(file.isFile()){
+
+                }
+                else {
+
                 }
             }
-        }
-        public static void receiveFile(String filename) throws Exception{
-            FileOutputStream fileOutputStream
-                    = new FileOutputStream(filename);
-            int bytes = 0;
-            FileInputStream fileInputStream = new FileInputStream(filename);
-            DataInputStream dataInputStream = new DataInputStream(fileInputStream);
-            long size = dataInputStream.readLong();
-            byte[] buffer = new byte[4 * 1024];
-            while (size > 0
-                    && (bytes = dataInputStream.read(
-                    buffer, 0,
-                    (int)Math.min(buffer.length, size)))
-                    != -1) {
-                // Here we write the file using write method
-                fileOutputStream.write(buffer, 0, bytes);
-                size -= bytes; // read upto file size
+            catch (IOException e){
+
             }
         }
+
     }
 
-
+    public static void receiveFile(String filename) throws Exception {
+        FileOutputStream fileOutputStream
+                = new FileOutputStream(filename);
+        int bytes = 0;
+        FileInputStream fileInputStream = new FileInputStream(filename);
+        DataInputStream dataInputStream = new DataInputStream(fileInputStream);
+        long size = dataInputStream.readLong();
+        byte[] buffer = new byte[4 * 1024];
+        while (size > 0
+                && (bytes = dataInputStream.read(
+                buffer, 0,
+                (int) Math.min(buffer.length, size)))
+                != -1) {
+            // Here we write the file using write method
+            fileOutputStream.write(buffer, 0, bytes);
+            size -= bytes; // read upto file size
+        }
+    }
 }
+
+
+
